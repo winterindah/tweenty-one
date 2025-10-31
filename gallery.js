e4bc635d9523423dab4fbea9dc2361e4fc686850
winterindah <mllsystfn@gmail.com>
2025-10-31 10:44:15 +0700

    Deploy web birthday cia

---
+++ gallery.js
@@ -0,0 +1,22 @@
+var TrandingSlider = new Swiper('.tranding-slider', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: 'auto',
+  initialSlide: 0,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 6.2,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
